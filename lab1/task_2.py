58df5950a037f1f4d05efbaa1c1edcbac7931853
Liv <146954549+Yulia-051@users.noreply.github.com>
2024-10-20 22:40:13 +0300

    Update task_2.py

--- lab1/task_2.py
+++ lab1/task_2.py
@@ -1,4 +1,10 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
 
-
-print("Количество книг, помещающихся на дискету:",round( 1.44*1024*1024//(100*50*25*4)))
+conversion = 1024
+max_memory_kb = 1.4 * conversion ** 2
+count_pages = 100
+cnt_strPage = 50
+cnt_symStr = 25
+sym_bytes = 4
+print("Количество книг, помещающихся на дискету:",
+      round(max_memory_kb / (count_pages * cnt_strPage * cnt_symStr * sym_bytes)))
