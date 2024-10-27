e470037ab89d2a087931cd300630dff32fece600
Liv <146954549+Yulia-051@users.noreply.github.com>
2024-10-27 21:56:18 +0300

    Update task_2.py

--- lab2/task_2.py
+++ lab2/task_2.py
@@ -5,10 +5,11 @@ increase = 0.05  # Ежемесячный рост цен
 count = 0
 # TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
 while 1:
-    if count != 0: spend *= (1 + increase)
-    money_capital -=spend - salary
-    count+=1
-    if money_capital<=0:
-        count-=1
+    if count != 0:
+        spend *= (1 + increase)
+    money_capital -= spend - salary
+    count += 1
+    if money_capital <= 0:
+        count -= 1
         break
 print("Количество месяцев, которое можно протянуть без долгов:", count)
